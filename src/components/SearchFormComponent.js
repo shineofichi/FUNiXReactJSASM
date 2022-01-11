43f2c3640efedf64527f61928ddb77a61214146b
Emuefujii Esusanbyakurokujyu (MFG S360) <030300AZ@jp.screen-net.local>
2022-01-11 10:39:57 +0900

    Search Staff was done

---
+++ src/components/SearchFormComponent.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Button, Form, FormGroup, Input } from "reactstrap";
+
+function SearchForm(props) {
+  return (
+    <div>
+      <Form inline>
+        <FormGroup>
+          <Input type="text" placeholder="Search" size="40"></Input>
+          <Button>
+            <span className="fa fa-search"></span>
+          </Button>
+        </FormGroup>
+      </Form>
+    </div>
+  );
+}
+
+export default SearchForm;
--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,22 +1,43 @@
-import React from "react";
-import { Card, CardImg, CardTitle } from "reactstrap";
+import React, { useState } from "react";
+import { Card, CardImg, CardTitle, Form, FormGroup, Input } from "reactstrap";
 import { Link } from "react-router-dom";
 
 function StaffList({ staffs }) {
-  const stafflist = staffs.map((staff) => {
-    return (
-      <div key={staff.id} className="col-12 col-md-4 col-lg-2 p-2">
-        <Link to={`/staffs/${staff.id}`}>
-          <Card>
-            <CardImg src={staff.image} alt={staff.name} />
-            <CardTitle className="text-center">{staff.name}</CardTitle>
-          </Card>
-        </Link>
-      </div>
-    );
-  });
+  const [searchName, setSearchName] = useState("");
+
+  const OnChangeSearchInput = (event) => {
+    setSearchName(event.currentTarget.value);
+  };
+  const stafflist = staffs
+    .filter((staff) =>
+      staff.name.toLowerCase().includes(searchName.toLowerCase())
+    )
+    .map((staff) => {
+      return (
+        <div key={staff.id} className="col-12 col-md-4 col-lg-2 p-2">
+          <Link to={`/staffs/${staff.id}`}>
+            <Card>
+              <CardImg src={staff.image} alt={staff.name} />
+              <CardTitle className="text-center">{staff.name}</CardTitle>
+            </Card>
+          </Link>
+        </div>
+      );
+    });
   return (
     <div className="container">
+      <div className="row">
+        <Form inline>
+          <FormGroup>
+            <Input
+              type="text"
+              placeholder="Tìm theo tên nhân viên"
+              size="40"
+              onChange={OnChangeSearchInput}
+            ></Input>
+          </FormGroup>
+        </Form>
+      </div>
       <div className="row">{stafflist}</div>
     </div>
   );
