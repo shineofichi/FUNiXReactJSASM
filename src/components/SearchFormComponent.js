db667aa069bf32f7d7a793c7a0f6c6abec9a1d62
Truong Tuan Anh <anhttfx13476@funix.edu.vn>
2022-01-17 19:58:45 +0900

    ASM3- Search button & Modal Initial

--- src/components/SearchFormComponent.js
+++ src/components/SearchFormComponent.js
@@ -3,7 +3,7 @@ import { Button, Form, FormGroup, Input } from "reactstrap";
 
 function SearchForm(props) {
   return (
-    <div>
+    <div className="ml-auto">
       <Form inline>
         <FormGroup>
           <Input type="text" placeholder="Search" size="40"></Input>
--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,13 +1,28 @@
 import React, { useState } from "react";
-import { Card, CardImg, CardTitle, Form, FormGroup, Input } from "reactstrap";
+import {
+  Button,
+  Card,
+  CardImg,
+  CardTitle,
+  Col,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Modal,
+  ModalBody,
+  ModalHeader,
+} from "reactstrap";
 import { Link } from "react-router-dom";
 
 function StaffList({ staffs }) {
   const [searchName, setSearchName] = useState("");
-
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const OnChangeSearchInput = (event) => {
-    setSearchName(event.currentTarget.value);
+    event.preventDefault();
+    setSearchName(event.target[0].value);
   };
+
   const stafflist = staffs
     .filter((staff) =>
       staff.name.toLowerCase().includes(searchName.toLowerCase())
@@ -27,16 +42,129 @@ function StaffList({ staffs }) {
   return (
     <div className="container">
       <div className="row">
-        <Form className="col-12 col-md-12 col-lg-6 pt-2">
+        <h1>Nhân viên</h1>
+        <Button
+          onClick={() => setIsModalOpen(!isModalOpen)}
+          color="primary"
+          className="m-auto"
+        >
+          <span className="fa fa-plus"></span>
+        </Button>
+        <Modal isOpen={isModalOpen}>
+          <ModalHeader toggle={() => setIsModalOpen(!isModalOpen)}>
+            Thêm nhân viên
+          </ModalHeader>
+          <ModalBody>
+            <Form>
+              <FormGroup row>
+                <Label htmlFor="name" md={4}>
+                  Tên
+                </Label>
+                <Col md={8}>
+                  <Input type="text" id="name" name="name"></Input>{" "}
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label htmlFor="doB" md={4}>
+                  Ngày sinh
+                </Label>
+                <Col md={8}>
+                  <Input type="date" id="doB" name="doB"></Input>
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label htmlFor="startDate" md={4}>
+                  Ngày vào công ty
+                </Label>{" "}
+                <Col md={8}>
+                  <Input type="date" id="startDate" name="startDate"></Input>{" "}
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label htmlFor="department" md={4}>
+                  Phòng ban
+                </Label>{" "}
+                <Col md={8}>
+                  <Input type="select" id="department" name="department">
+                    <option>Sale</option>
+                    <option>HR</option>
+                    <option>Marketing</option>
+                    <option>IT</option>
+                    <option>Finance</option>
+                  </Input>{" "}
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label htmlFor="salaryScale" md={4}>
+                  Hệ số lương
+                </Label>{" "}
+                <Col md={8}>
+                  <Input
+                    type="text"
+                    id="salaryScale"
+                    name="salaryScale"
+                    defaultValue="1"
+                  ></Input>{" "}
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label htmlFor="annualLeave" md={4}>
+                  Số ngày nghỉ còn lại
+                </Label>{" "}
+                <Col md={8}>
+                  <Input
+                    type="text"
+                    id="annualLeave"
+                    name="annualLeave"
+                    defaultValue="0"
+                  ></Input>{" "}
+                </Col>
+              </FormGroup>
+              <FormGroup row>
+                <Label htmlFor="overTime" md={4}>
+                  Số ngày đã làm thêm
+                </Label>{" "}
+                <Col md={8}>
+                  <Input
+                    type="text"
+                    id="overTime"
+                    name="overTime"
+                    defaultValue="0"
+                  ></Input>{" "}
+                </Col>
+              </FormGroup>
+              <FormGroup>
+                <Button
+                  type="submit"
+                  value="submit"
+                  color="primary"
+                  className="offset-5"
+                >
+                  Thêm
+                </Button>
+              </FormGroup>
+            </Form>
+          </ModalBody>
+        </Modal>
+        <Form className="pt-2 ml-auto" inline onSubmit={OnChangeSearchInput}>
           <FormGroup>
+            <Label htmlFor="searchtext"></Label>
             <Input
               type="text"
               placeholder="Tìm theo tên nhân viên"
-              onChange={OnChangeSearchInput}
-            ></Input>
+              size="30"
+              id="seachtext"
+              name="searchtext"
+            />
+          </FormGroup>
+          <FormGroup>
+            <Button type="submit" value="submit" color="primary">
+              Tìm
+            </Button>
           </FormGroup>
         </Form>
       </div>
+      <hr></hr>
       <div className="row">{stafflist}</div>
     </div>
   );
