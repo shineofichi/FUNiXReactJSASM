b0028c7faa122da930e639f2c8d219f1870095b5
Truong Tuan Anh <anhttfx13476@funix.edu.vn>
2022-02-06 22:36:53 +0900

    Conver Main Component to Functional Component

--- src/components/Container/MainComponent.js
+++ src/components/Container/MainComponent.js
@@ -1,11 +1,10 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import {
   Route,
   Routes,
   useParams,
   Navigate,
   useLocation,
-  useNavigate,
 } from "react-router-dom";
 import StaffList from "../Presentational/StaffListComponent";
 import Header from "../Presentational/HeaderComponent";
@@ -25,19 +24,6 @@ import {
 } from "../../redux/ActionCreator";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 
-// WRAPPER  withRouter for use Location
-// Source : https://reactrouter.com/docs/en/v6/faq
-function withRouter(Component) {
-  function ComponentWithRouterProp(props) {
-    let location = useLocation();
-    let navigate = useNavigate();
-    let params = useParams();
-    return <Component {...props} router={{ location, navigate, params }} />;
-  }
-
-  return ComponentWithRouterProp;
-}
-
 const mapStateToProps = (state) => {
   return {
     staffs: state.staffs,
@@ -55,84 +41,78 @@ const mapDispathToProps = (dispath) => ({
   editStaffInfo: (staff) => dispath(editStaffInfo(staff)),
 });
 
-class Main extends Component {
-  componentDidMount() {
-    this.props.fetchStaffs();
-    this.props.fetchDepts();
-    this.props.fetchSalary();
-  }
-  render() {
-    const StaffWithId = () => {
-      const params = useParams();
-      return (
-        <StaffDetail
-          staff={
-            this.props.staffs.staffs.filter(
-              (staff) => staff.id === parseInt(params.id, 10)
-            )[0]
-          }
-          departments={this.props.departments}
-        />
-      );
-    };
-    const DepDetailWithId = () => {
-      const params = useParams();
-      return (
-        <DeptDetail
-          staffs={this.props.staffs.staffs.filter(
-            (staff) => staff.departmentId === params.id
-          )}
-          department={
-            this.props.departments.departments.filter(
-              (dept) => dept.id === params.id
-            )[0]
-          }
-        />
-      );
-    };
+function Main(props) {
+  useEffect(() => {
+    props.fetchStaffs();
+    props.fetchDepts();
+    props.fetchSalary();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  const location = useLocation();
+  const StaffWithId = () => {
+    const params = useParams();
     return (
-      <div>
-        <Header />
-        <TransitionGroup>
-          <CSSTransition
-            key={this.props.router.location.key}
-            classNames="page"
-            timeout={300}
-          >
-            <Routes location={this.props.router.location}>
-              <Route
-                path="/staffs"
-                element={
-                  <StaffList
-                    staffs={this.props.staffs.staffs}
-                    isLoading={this.props.staffs.isStaffLoading}
-                    errMess={this.props.staffs.errStaffMess}
-                    postAddNewStaff={this.props.postAddNewStaff}
-                    deleteStaff={this.props.deleteStaff}
-                    editStaffInfo={this.props.editStaffInfo}
-                  />
-                }
-              />
-              <Route path="/staffs/:id" element={<StaffWithId />} />
-              <Route
-                path="/dep"
-                element={
-                  <Department deps={this.props.departments.departments} />
-                }
-              />
-              <Route path="/dep/:id" element={<DepDetailWithId />} />
-              <Route
-                path="/salary"
-                element={<Salary salary={this.props.salary.salary} />}
-              />
-              <Route path="*" element={<Navigate to="/staffs" />} />
-            </Routes>
-          </CSSTransition>
-        </TransitionGroup>
-        <Footer />
-      </div>
+      <StaffDetail
+        staff={
+          props.staffs.staffs.filter(
+            (staff) => staff.id === parseInt(params.id, 10)
+          )[0]
+        }
+        departments={props.departments}
+      />
     );
-  }
+  };
+  const DepDetailWithId = () => {
+    const params = useParams();
+    return (
+      <DeptDetail
+        staffs={props.staffs.staffs.filter(
+          (staff) => staff.departmentId === params.id
+        )}
+        department={
+          props.departments.departments.filter(
+            (dept) => dept.id === params.id
+          )[0]
+        }
+      />
+    );
+  };
+  return (
+    <div>
+      <Header />
+      <TransitionGroup>
+        <CSSTransition key={location.key} classNames="page" timeout={300}>
+          <Routes location={location}>
+            <Route
+              path="/staffs"
+              element={
+                <StaffList
+                  staffs={props.staffs.staffs}
+                  isLoading={props.staffs.isStaffLoading}
+                  errMess={props.staffs.errStaffMess}
+                  postAddNewStaff={props.postAddNewStaff}
+                  deleteStaff={props.deleteStaff}
+                  editStaffInfo={props.editStaffInfo}
+                />
+              }
+            />
+            <Route path="/staffs/:id" element={<StaffWithId />} />
+            <Route
+              path="/dep"
+              element={<Department deps={props.departments.departments} />}
+            />
+            <Route path="/dep/:id" element={<DepDetailWithId />} />
+            <Route
+              path="/salary"
+              element={<Salary salary={props.salary.salary} />}
+            />
+            <Route path="*" element={<Navigate to="/staffs" />} />
+          </Routes>
+        </CSSTransition>
+      </TransitionGroup>
+      <Footer />
+    </div>
+  );
 }
 
-export default withRouter(connect(mapStateToProps, mapDispathToProps)(Main));
+export default connect(mapStateToProps, mapDispathToProps)(Main);
